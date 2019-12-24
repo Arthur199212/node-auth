513767cf1fd824a2865b3a889378921b015039cb
Artur Rubchenko <2146596@mail.ru>
2019-12-24 08:46:27 +0300

    feat(api): add redis and mongoDB

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,24 +1,39 @@
+import mongoose from 'mongoose'
 import express from 'express'
 import session from 'express-session'
 import Redis from 'ioredis'
 import connectRedis from 'connect-redis'
-import { APP_PORT } from './config'
+import {
+  APP_PORT,
+  REDIS_OPTIONS,
+  SESSION_OPTIONS,
+  MONGO_URI,
+  MONGO_OPTIONS
+} from './config'
 
-const app = express()
-
-const RedisStore = connectRedis(session)
-const client = new Redis()
-
-app.use(
-  session({
-    store: new RedisStore({ client }),
-    secret: 'keyboard cat',
-    resave: false,
-  })
-)
-
-app.get('/', (req, res) => {
-  res.json({ message: 'Hey' })
-})
-
-app.listen(APP_PORT, () => console.log(`http://localhost:${APP_PORT}/`))
+;(async () => {
+  try {
+    await mongoose.connect(MONGO_URI, MONGO_OPTIONS)
+  
+    const RedisStore = connectRedis(session)
+    
+    const client = new Redis(REDIS_OPTIONS)
+  
+    const app = express()
+  
+    app.use(
+      session({
+        store: new RedisStore({ client }),
+        ...SESSION_OPTIONS
+      })
+    )
+  
+    app.get('/', (req, res) => {
+      res.json({ message: 'Hey' })
+    })
+  
+    app.listen(APP_PORT, () => console.log(`http://localhost:${APP_PORT}/`))
+  } catch (err) {
+    console.log(err)
+  }
+})()
