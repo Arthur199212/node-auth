97f33b94054af1c2ab847da65dbcc37fc98d7117
Artur Rubchenko <2146596@mail.ru>
2019-12-26 16:38:04 +0300

    chore: delete comment

--- api/src/middleware/auth.ts
+++ api/src/middleware/auth.ts
@@ -3,7 +3,6 @@ import { isLoggedIn } from '../auth'
 
 export const guest = (req: Request, res: Response, next: NextFunction) => {
   if (isLoggedIn(req)) {
-    console.log('HEY!')
     return next(new Error('You are already logged in'))
   }
 
