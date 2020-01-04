61466146776ac79785696488441ae9006f0cb4b9
Artur Rubchenko <2146596@mail.ru>
2020-01-04 10:24:49 +0300

    feat: add password hashing

--- api/src/config/index.ts
+++ api/src/config/index.ts
@@ -1,4 +1,9 @@
 export * from './app'
+
+export * from './auth'
+
 export * from './db'
+
 export * from './cache'
+
 export * from './session'
--- api/src/validation/auth.ts
+++ api/src/validation/auth.ts
@@ -1,22 +1,33 @@
 import Joi from '@hapi/joi'
+import { BCRYPT_MAX_BYTES } from '../config'
+
+const email = Joi.string()
+  .email()
+  .min(8)
+  .max(254)
+  .lowercase()
+  .trim()
+  .required()
+
+const name = Joi.string()
+  .min(3)
+  .max(128)
+  .trim()
+  .required()
+
+const password = Joi.string()
+  .min(8)
+  .max(BCRYPT_MAX_BYTES, 'utf8')
+  .regex(/^(?=.*?[\p{Lu}])(?=.*?[\p{Ll}])(?=.*?\d).*$/u)
+  .message('"{#label}" must contain one uppercase letter, one lowercase letter, and one digit')
+  .required()
+
+const passwordConfirmation = Joi.valid(Joi.ref('password'))
+  .required()
 
 export const registerSchema = Joi.object({
-  email: Joi.string()
-    .email()
-    .min(8)
-    .max(254)
-    .lowercase()
-    .trim()
-    .required(),
-  name: Joi.string()
-    .min(3)
-    .max(128)
-    .trim()
-    .required(),
-  password: Joi.string()
-    .min(8)
-    .max(128)
-    .required(),
-  passwordConfirmation: Joi.valid(Joi.ref('password'))
-    .required(),
+  email,
+  name,
+  password,
+  passwordConfirmation,
 })
